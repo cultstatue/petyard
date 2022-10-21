37e77f3098ee830bbd69aad94960939afe618a0c
cultstatue <saltedchurros@gmail.com>
2022-10-21 11:44:37 -0400

    added most of form functionality for sign up

--- client/src/components/Login/index.js
+++ client/src/components/Login/index.js
@@ -3,15 +3,39 @@ import { Form, Button, Alert } from 'react-bootstrap';
 
 function Login() {
 	return (
-
+		<>
+		<Form>
 		<Form.Group>
 			<Form.Label htmlFor='email'>Email</Form.Label>
 			<Form.Control
 			type='text'
 			placeholder='your email'
 			name='email'
+			required
 			/>
+
+		 <Form.Control.Feedback type='invalid'>Please enter an email address!</Form.Control.Feedback>
 		</Form.Group>
+
+		<Form.Group>
+			<Form.Control
+			type='text'
+			placeholder='your password'
+			name='password'
+			required
+			/>
+			<Form.Control.Feedback type='invalid'>Please enter a password!</Form.Control.Feedback>
+		</Form.Group>
+
+		<Button
+        //   disabled={!(userFormData.email && userFormData.password)}
+          type='submit'
+          variant='success'>
+          Submit
+        </Button>
+		</Form>
+
+		</>
 
 	);
 }
--- client/src/components/Signup/index.js
+++ client/src/components/Signup/index.js
@@ -1,17 +1,114 @@
 import React, { useState } from "react";
 import { Form, Button, Alert } from 'react-bootstrap';
 
+import { useMutation } from '@apollo/client';
+import { ADD_USER } from '../../utils/mutations';
+
+import Auth from '../../utils/auth';
 function Signup() {
+
+	const [userFormData, setUserFormData] = useState({ username: '', email: '', password: '' });
+
+	const [validated] = useState(false);
+
+	const [showAlert, setShowAlert] = useState(false);
+
+	const [addUser, { error }] = useMutation(ADD_USER);
+
+	const handleInputChange = (event) => {
+		const { name, value } = event.target;
+		setUserFormData({ ...userFormData, [name]: value });
+	};
+	
+	const formHandler = async(event) => {
+		event.preventDefault();
+
+		const form = event.currentTarget;
+		if (form.checkValidity() === false) {
+			event.preventDefault();
+			event.stopPropagation();
+		}
+		
+	    try {
+			const { data } = await addUser({
+			  variables: { ...userFormData,}
+			})
+	  
+			console.log(data);
+	  
+			Auth.login(data.addUser.token);
+	  
+		} catch (e) {
+			console.error(e);
+		}
+	  
+		  setUserFormData({
+			username: '',
+			email: '',
+			password: '',
+		});
+	};
+
 	return (
 
-		<Form.Group>
-			<Form.Label htmlFor='email'>Email</Form.Label>
-			<Form.Control
-			type='text'
-			placeholder='your email'
-			name='email'
-			/>
-		</Form.Group>
+		<Form noValidate validated={validated} onSubmit={formHandler}>
+
+		<Alert dismissible onClose={() => setShowAlert(false)} show={showAlert} variant='danger'>
+				Something went wrong!å
+        </Alert>
+
+			<Form.Group classname="mb-3" controlId="formEmail">
+				<Form.Text className="text">
+					Enter an email address
+				</Form.Text>
+				<Form.Control 
+				type="email" 
+				placeholder="Email"
+				onChange={handleInputChange}
+				value={userFormData.email}
+				required
+				/>
+
+				<Form.Control.Feedback type='invalid'>Please enter an email address!</Form.Control.Feedback>
+			</Form.Group>
+
+			<Form.Group classname="mb-3" controlId="formUsername">
+				<Form.Text className="text">
+					Create a username
+				</Form.Text>
+				<Form.Control 
+				type="username" 
+				placeholder="Username"
+				onChange={handleInputChange}
+            	value={userFormData.username}
+				required
+				/>
+
+				<Form.Control.Feedback type='invalid'>Please enter a username!</Form.Control.Feedback>
+			</Form.Group>
+
+			<Form.Group classname="mb-3" controlId="formPassword">
+				<Form.Text className="text">
+					Create a password
+				</Form.Text>
+				<Form.Control 
+				type="password" 
+				placeholder="Password" 
+				onChange={handleInputChange}
+				value={userFormData.password}
+				required
+				/>
+
+				<Form.Control.Feedback type='invalid'>Please enter a username!</Form.Control.Feedback>
+			</Form.Group>
+			
+			<Form.Group className="mb-3" controlId="placeholder">
+				
+			</Form.Group>
+			<Button variant="primary" type="submit">
+				Submit
+			</Button>
+		</Form>
 
 	);
 }
