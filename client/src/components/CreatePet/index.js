34838e95fd44b0e7e4c4731ba01d78d1c92dfa63
Claire Cashmore <clairecashmore2017@gmail.com>
2022-10-25 22:12:08 -0400

    removed contact (doesnt serve any function)

--- client/src/components/CreatePet/index.js
+++ client/src/components/CreatePet/index.js
@@ -63,121 +63,120 @@ function CreatePet() {
       breed: "",
       image: "cat.gif",
     });
-    window.location.replace("/profile");
+    //This has some issues
+    window.location.redirect("/profile");
   };
 
   return (
     <>
-    <Container fluid>
-      <Row>
-       
-        
-        <Col>
-        <div classname="form-container">
-          <h1> Add a Pet! </h1>
-          <Form noValidate validated={validated} onSubmit={formHandler}>
-            <Form.Group className="mb-3" controlId="formPetName">
-              <Form.Label>Pet's name</Form.Label>
-              <Form.Control
-                name="name"
-                onChange={handleInputChange}
-                value={petFormData.name}
-                type="text"
-                placeholder="Your Pet's name"
-              />
-            </Form.Group>
+      <Container fluid>
+        <Row>
+          <Col>
+            <div classname="form-container">
+              <h1> Add a Pet! </h1>
+              <Form noValidate validated={validated} onSubmit={formHandler}>
+                <Form.Group className="mb-3" controlId="formPetName">
+                  <Form.Label>Pet's name</Form.Label>
+                  <Form.Control
+                    name="name"
+                    onChange={handleInputChange}
+                    value={petFormData.name}
+                    type="text"
+                    placeholder="Your Pet's name"
+                  />
+                </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formPetGender">
-              <Form.Label>Pet's Gender</Form.Label>
-              <Form.Select
-                name="gender"
-                onChange={handleInputChange}
-                value={petFormData.gender}
-              >
-                <option>Male</option>
-                <option>Female</option>
-                <option>Unknown</option>
-              </Form.Select>
-            </Form.Group>
+                <Form.Group className="mb-3" controlId="formPetGender">
+                  <Form.Label>Pet's Gender</Form.Label>
+                  <Form.Select
+                    name="gender"
+                    onChange={handleInputChange}
+                    value={petFormData.gender}
+                  >
+                    <option>Male</option>
+                    <option>Female</option>
+                    <option>Unknown</option>
+                  </Form.Select>
+                </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formPetAge">
-              <Form.Label>Pet's Age</Form.Label>
-              <Form.Control
-                name="age"
-                type="number"
-                onChange={handleInputChange}
-                value={petFormData.age}
-                placeholder="Your Pet's age"
-              />
-            </Form.Group>
+                <Form.Group className="mb-3" controlId="formPetAge">
+                  <Form.Label>Pet's Age</Form.Label>
+                  <Form.Control
+                    name="age"
+                    type="number"
+                    onChange={handleInputChange}
+                    value={petFormData.age}
+                    placeholder="Your Pet's age"
+                  />
+                </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formPetSpecies">
-              <Form.Label>Pet's Species</Form.Label>
-              <Form.Select
-                name="species"
-                onChange={handleInputChange}
-                value={petFormData.species}
-              >
-                <option>Dog</option>
-                <option>Cat</option>
-              </Form.Select>
-            </Form.Group>
+                <Form.Group className="mb-3" controlId="formPetSpecies">
+                  <Form.Label>Pet's Species</Form.Label>
+                  <Form.Select
+                    name="species"
+                    onChange={handleInputChange}
+                    value={petFormData.species}
+                  >
+                    <option>Dog</option>
+                    <option>Cat</option>
+                  </Form.Select>
+                </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formPetBreed">
-              <Form.Label>Pet's Breed</Form.Label>
-              <Form.Control
-                name="breed"
-                type="text"
-                onChange={handleInputChange}
-                value={petFormData.breed}
-                placeholder="Your Pet's Breed"
-              />
-            </Form.Group>
+                <Form.Group className="mb-3" controlId="formPetBreed">
+                  <Form.Label>Pet's Breed</Form.Label>
+                  <Form.Control
+                    name="breed"
+                    type="text"
+                    onChange={handleInputChange}
+                    value={petFormData.breed}
+                    placeholder="Your Pet's Breed"
+                  />
+                </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formPetImage">
-              <Form.Label>Choose an image for your pet!</Form.Label>
+                <Form.Group className="mb-3" controlId="formPetImage">
+                  <Form.Label>Choose an image for your pet!</Form.Label>
 
-              <div
-                onChange={handleInputChange}
-                defaultValue={petFormData.image}
-              >
-                <input type="radio" value="cat.gif" name="image" />
-                <img className="pet" src={`/images/cat.gif`}></img>
+                  <div
+                    onChange={handleInputChange}
+                    defaultValue={petFormData.image}
+                  >
+                    <input type="radio" value="cat.gif" name="image" />
+                    <img className="pet" src={`/images/cat.gif`}></img>
 
-                <input type="radio" value="cat2.gif" name="image" />
-                <img className="pet" src={`/images/cat2.gif`}></img>
+                    <input type="radio" value="cat2.gif" name="image" />
+                    <img className="pet" src={`/images/cat2.gif`}></img>
 
-                <input type="radio" value="dog.gif" name="image" />
-                <img className="pet" src={`/images/dog.gif`}></img>
+                    <input type="radio" value="dog.gif" name="image" />
+                    <img className="pet" src={`/images/dog.gif`}></img>
 
-                <input type="radio" value="dog2.gif" name="image" />
-                <img className="pet" src={`/images/dog2.gif`}></img>
+                    <input type="radio" value="dog2.gif" name="image" />
+                    <img className="pet" src={`/images/dog2.gif`}></img>
 
-                <input type="radio" value="dog3.gif" name="image" />
-                <img className="pet" src={`/images/dog3.gif`}></img>
-              </div>
-            </Form.Group>
+                    <input type="radio" value="dog3.gif" name="image" />
+                    <img className="pet" src={`/images/dog3.gif`}></img>
+                  </div>
+                </Form.Group>
 
-            <Button
-              disabled={
-                !(
-                  petFormData.image &&
-                  petFormData.breed &&
-                  petFormData.species &&
-                  petFormData.age &&
-                  petFormData.gender &&
-                  petFormData.name
-                )
-              }
-              type="submit"
-              variant="success"
-            >
-              Create!
-            </Button>
-          </Form>
-          </div>
-        </Col>
-      </Row>
+                <Button
+                  disabled={
+                    !(
+                      petFormData.image &&
+                      petFormData.breed &&
+                      petFormData.species &&
+                      petFormData.age &&
+                      petFormData.gender &&
+                      petFormData.name
+                    )
+                  }
+                  type="submit"
+                  variant="success"
+                >
+                  Create!
+                </Button>
+              </Form>
+            </div>
+          </Col>
+        </Row>
       </Container>
     </>
   );
